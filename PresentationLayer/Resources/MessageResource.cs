6e10a3acc3e48137d555c4a686451741597ed20e
Moe Thet Khine <soniapoudel03@gmail.com>
2024-06-26 18:06:18 +0630

    -

--- PresentationLayer/Resources/MessageResource.cs
+++ PresentationLayer/Resources/MessageResource.cs
@@ -6,6 +6,5 @@
         public static string SaveFail { get; } = "Saving Fail";
         public static string DeleteSuccess { get; } = "Deleting Successful";
         public static string DeleteFail { get; } = "Deleting Fail";
-
     }
 }
