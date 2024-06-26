cc148257ed3bfba59bc81d149377c06fa73b8063
Moe Thet Khine <soniapoudel03@gmail.com>
2024-06-26 18:06:47 +0630

    -

--- PresentationLayer/Program.cs
+++ PresentationLayer/Program.cs
@@ -4,7 +4,6 @@ using PresentationLayer.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
