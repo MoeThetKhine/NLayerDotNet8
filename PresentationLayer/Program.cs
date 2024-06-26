f2ce4acd232dc4c67f0113c62a237576d8108d0a
Moe Thet Khine <soniapoudel03@gmail.com>
2024-06-26 18:07:46 +0630

    -

--- PresentationLayer/Program.cs
+++ PresentationLayer/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using PresentationLayer.Service;
 
 var builder = WebApplication.CreateBuilder(args);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
